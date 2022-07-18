356bf53b4aff73a4fa7e1ff999a206d649933f2c
Paulo Afonso <pauloferrisouza@gmail.com>
2022-07-17 21:49:46 -0300

    feat: created logo to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import walletIcon from '../images/wallet_icon.png'
 import './CSS/Login.css';
 
 const Login = () => {
   return (
     <div className="login_page" >
       <div className="animation_login_container">
-      <iframe title="investment animation" src="https://embed.lottiefiles.com/animation/73295" />
+      <div className="login_logo">
+        <img src={walletIcon} alt="icon representing a wallet" />
+        <h1>XP WALLET</h1>
+      </div>
+        <iframe title="investment animation" src="https://embed.lottiefiles.com/animation/73295" />
       </div>
       <div className="login_container">
-        <h1>
+        <h2>
           Acesse a sua conta!
-        </h1>
+        </h2>
         <form className="login_form_container">
           <label htmlFor="email">
             <input
